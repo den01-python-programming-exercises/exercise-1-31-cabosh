f10eb7abba254217390cc4ba95b3f9e2a14bec51
cabosh <callum.bosher@gmail.com>
2020-06-03 15:48:00 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    year = int(input("Give a year: "))
+
+    if (year % 4) == 0:
+        print(f"The year {year} is a leap year!")
+    else:
+        print(f"The year {year} is not a leap year.")
 
 if __name__ == '__main__':
     main()
